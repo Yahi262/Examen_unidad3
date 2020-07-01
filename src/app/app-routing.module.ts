877797c097c6be61259b76d8bf6bae41aa0a3f88
Yahi09 <valenciayahaira262@gmail.com>
2020-07-01 16:18:53 -0500

    creacion de componentes y navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TareasComponent } from './componentes/tareas/tareas.component';
+import { NotasComponent } from './componentes/notas/notas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'notas',component:NotasComponent},
+  {path: 'tareas',component:TareasComponent},
+  {path: '**',pathMatch:'full',redirectTo:'notas'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { TareasComponent } from './componentes/tareas/tareas.component';
+import { NotasComponent } from './componentes/notas/notas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TareasComponent,
+    NotasComponent
   ],
   imports: [
     BrowserModule,
